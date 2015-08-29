9b9e1c44519cc233d9a7285bae2ac3813052fdaf
Manfred Tremmel <Manfred.Tremmel@iiv.de>
2015-08-29 23:24:29 +0200

    0.8.8: update to gwt-bean-validators 0.8.8 separated iban input field to a widget

--- src/main/java/de/knightsoftnet/validationexample/client/ui/page/sepa/SepaViewGwtImpl.java
+++ src/main/java/de/knightsoftnet/validationexample/client/ui/page/sepa/SepaViewGwtImpl.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -22,7 +22,6 @@ import de.knightsoftnet.validators.client.decorators.UniversalDecoratorWithIcons
 import de.knightsoftnet.validators.client.editor.BeanValidationEditorDriver;
 import de.knightsoftnet.validators.client.event.FormSubmitEvent;
 import de.knightsoftnet.validators.client.event.FormSubmitHandler;
-import de.knightsoftnet.validators.client.handlers.HandlerFactory;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -125,8 +124,6 @@ public class SepaViewGwtImpl extends Composite implements SepaViewInterface {
         SepaViewGwtImpl.this.activity.tryToSend();
       }
     });
-    this.iban.addKeyPressHandler(HandlerFactory.getIbanKeyPressHandler());
-    this.iban.addKeyUpHandler(HandlerFactory.getIbanKeyUpHandler());
     ((BicWidget) this.bic.getWidget()).setBankNameWidget(this.bankName);
   }
 
