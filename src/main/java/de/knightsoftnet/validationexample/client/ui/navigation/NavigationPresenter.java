289373c5d276305044c2a217fa881be2a82eb92d
Manfred Tremmel <Manfred.Tremmel@iiv.de>
2015-10-01 20:33:45 +0200

    fixed menu handling when user is logged in and page is reloaded

--- src/main/java/de/knightsoftnet/validationexample/client/ui/navigation/NavigationPresenter.java
+++ src/main/java/de/knightsoftnet/validationexample/client/ui/navigation/NavigationPresenter.java
@@ -4,9 +4,9 @@
  * copyright ownership. The ASF licenses this file to You under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance with the License. You may obtain a
  * copy of the License at
- *
+ * 
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * 
  * Unless required by applicable law or agreed to in writing, software distributed under the License
  * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
  * or implied. See the License for the specific language governing permissions and limitations under
@@ -61,7 +61,7 @@ public class NavigationPresenter extends
 
     /**
      * set new active entry.
-     * 
+     *
      * @param pnewEntry new active entry
      */
     void setSelectedItem(final NavigationEntryInterface pnewEntry);
@@ -118,7 +118,8 @@ public class NavigationPresenter extends
     } else {
       if (this.placeManager.getHierarchyDepth() > 1) {
         this.placeManager.revealRelativePlace(-1);
-      } else {
+      } else if (StringUtils.equals(this.placeManager.getCurrentPlaceRequest().getNameToken(),
+          NameTokens.LOGIN)) {
         this.placeManager.revealDefaultPlace();
       }
     }
